bcba237b6a013e04e8620611bb291e3fdaba7506
floydianlou <yukiyesakura@gmail.com>
2025-02-25 23:44:58 +0100

    Added methods to DAOInterface and AccountManager

--- SWEMagicShopProject/src/BusinessLogic/AccountManager.java
+++ SWEMagicShopProject/src/BusinessLogic/AccountManager.java
@@ -1,32 +1,39 @@
 package BusinessLogic;
 
 import DAOInterface.AccountDAO;
+import DomainModel.Customer;
 import DomainModel.Person;
 import DomainModel.Species;
+import java.util.ArrayList;
 
 public class AccountManager {
     Person person;
     AccountDAO accountDAO; // TO BE INCLUDED IN METHODS WHEN DAO IS IMPLEMENTED
 
-    public AccountManager (Person person) {
+    public AccountManager (Person person, AccountDAO accountDAO) {
         this.person = person;
+        this.accountDAO = accountDAO;
     }
 
+    // TODO: after DAO, exceptions need to be managed, functions need to be re-checked.
     // methods
-    public Person login (String email, String password) { // TODO: after DAO, exceptions need to be managed
-        // here we create a connection with DAO...
-
-        // this function should return a method from AccountManagerDAO which checks in the database for
-        // a match with the given email and password, returning a Person - Customer or Manager - to activate
-        // their session.
-        return person;  // here we use a method from DAO, leaving this return person as a placeholder.
+    public Person login (String email, String password) {
+        return accountDAO.loginPerson(email, password);
     }
 
     public void createCustomerAccount (String name, String surname, String email, String password, int age,
                                   int phoneNumber, Species species) {
         // WARNING: not exactly sure how to manage this Species object thing yet...so it's probably due to be changed soon.
+        accountDAO.createCustomerAccount(name, surname, email, password, age, phoneNumber, species);
     }
 
+    public void createManagerAccount (String name, String surname, String email, String password) {
+        // WARNING: not exactly sure how to manage this Species object thing yet...so it's probably due to be changed soon.
+        accountDAO.createManagerAccount(name, surname, email, password);
+    }
 
+    public ArrayList<Customer> showAllCustomers() {
+        return accountDAO.viewAllCustomers();
+    }
 
 }
--- SWEMagicShopProject/src/DAOInterface/AccountDAO.java
+++ SWEMagicShopProject/src/DAOInterface/AccountDAO.java
@@ -1,11 +1,20 @@
 package DAOInterface;
 
+import DomainModel.Person;
+import DomainModel.Customer;
+import DomainModel.Species;
+
+import java.util.ArrayList;
+
 // JUST AN INTERFACE TO MAKE BUSINESS LOGIC FIRST !!
 public interface AccountDAO {
     public void createCustomerAccount (String name, String surname, String email, String password, int age,
-                                       int phoneNumber);
+                                       int phoneNumber, Species species);
     // this function should check for existent customer emails (maybe return boolean then??) then create account
     public void createManagerAccount (String name, String surname, String email, String password);
     // same as above
+
+    public Person loginPerson (String email, String password);
+    public ArrayList<Customer> viewAllCustomers();
 
 }
