4955f14f20e8a215ff7010644a3e3d5bca078ba0
floydianlou <yukiyesakura@gmail.com>
2025-03-03 12:31:19 +0100

    Modified Item and Order to remove GP and SP
    Also added some methods to CustomerOrderManager

--- SWEMagicShopProject/src/DAOInterface/OrderDAO.java
+++ SWEMagicShopProject/src/DAOInterface/OrderDAO.java
@@ -1,4 +1,12 @@
 package DAOInterface;
 
+import DomainModel.Customer;
+import DomainModel.Order;
+
+import java.util.ArrayList;
+
 public interface OrderDAO {
+
+    ArrayList<Order> getAllOrders();
+    ArrayList<Order> getCustomerOrders(Customer customer);
 }
