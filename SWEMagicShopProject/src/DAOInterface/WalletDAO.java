5c9f45a92929394ab1ed4d3c84d06397339325a5
Novalynne <schirone.ma@gmail.com>
2025-03-04 20:03:22 +0100

    Created StoreManager class and ItemDAO interface
    Created some functions for these classes

--- SWEMagicShopProject/src/DAOInterface/WalletDAO.java
+++ SWEMagicShopProject/src/DAOInterface/WalletDAO.java
@@ -7,7 +7,7 @@ public interface WalletDAO {
 
     //there should be a function to create a wallet but I still need to figure out
     // when it shall be called. Maybe in the creation of the customer account?
-    public Wallet createWalletForCustomer(Customer customer);
+    public void createWalletForCustomer(Customer customer);
     public Wallet getWalletByID(int ID);
     public void updateWallet(Wallet wallet);
 
