a183f5fab090c1178d785cb4cebaf99c0c5af7f8
Novalynne <schirone.ma@gmail.com>
2025-03-04 19:15:37 +0100

    New function added to WalletDAO
    createWalletForCustomer added.

--- SWEMagicShopProject/src/DAOInterface/WalletDAO.java
+++ SWEMagicShopProject/src/DAOInterface/WalletDAO.java
@@ -1,9 +1,13 @@
 package DAOInterface;
 
+import DomainModel.Customer;
 import DomainModel.Wallet;
 
 public interface WalletDAO {
 
+    //there should be a function to create a wallet but I still need to figure out
+    // when it shall be called. Maybe in the creation of the customer account?
+    public Wallet createWalletForCustomer(Customer customer);
     public Wallet getWalletByID(int ID);
     public void updateWallet(Wallet wallet);
 
