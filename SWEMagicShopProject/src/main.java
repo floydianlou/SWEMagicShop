bc0777cd45ea457123f91ac3e5c35cbc96261344
floydianlou <yukiyesakura@gmail.com>
2025-03-07 01:21:41 +0100

    Added connectionManager class and main test
    Class needs to be completed still. DO NOT USE YET

--- SWEMagicShopProject/src/main.java
+++ SWEMagicShopProject/src/main.java
@@ -1,6 +1,20 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class main {
     public static void main(String[] args) {
+        String url = "jdbc:postgresql://localhost:5432/otariMagicShop";
+        String username = "postgres";
+        String password = "postgres";
 
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            if (connection != null) {
+                System.out.println("Successfully connected to PostgreSQL database!");
+            }
+        } catch (SQLException e) {
+            System.out.println("Warning: " + e.getMessage());
+        }
     }
-
 }
+
