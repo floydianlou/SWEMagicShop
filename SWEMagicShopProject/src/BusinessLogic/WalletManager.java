e942e4e8fd83640c032c73777c8373c3d40e02eb
Novalynne <schirone.ma@gmail.com>
2025-03-15 19:49:49 +0100

    Corrected WalletManager to match new WalletDAO

--- SWEMagicShopProject/src/BusinessLogic/WalletManager.java
+++ SWEMagicShopProject/src/BusinessLogic/WalletManager.java
@@ -1,11 +1,11 @@
 package BusinessLogic;
 
-import DAOInterface.WalletDAO;
+import ORM.WalletDAO;
 import DomainModel.Customer;
 import DomainModel.Wallet;
 
 public class WalletManager {
-    WalletDAO walletDAO; //TODO: TO BE INCLUDED IN METHODS WHEN DAO IS IMPLEMENTED, CAN'T BE IMPLEMENTED NOW SINCE WE'RE USING AN INTERFACE
+    WalletDAO walletDAO;
 
     public WalletManager(WalletDAO walletDAO) {
         this.walletDAO = walletDAO;
@@ -15,10 +15,22 @@ public class WalletManager {
         // FOR TESTING ONLY
     }
 
+    //function to see balance
+    public int viewBalance(int id) {
+        WalletDAO walletDAO = new WalletDAO();
+        Wallet wallet = walletDAO.getWalletByID(id);
+
+        if (wallet == null) {
+            System.out.println("Wallet not found for this customer.");
+            return -1;
+        }
+
+        return wallet.getCPbalance();
+    }
 
     //function to add and widthdraw money from the wallet
     public void addFunds(int amountGP, int amountSP, int amountCP, Customer customer) {
-        // WalletDAO walletDAO = new WalletDAO();
+        WalletDAO walletDAO = new WalletDAO();
         Wallet wallet = walletDAO.getWalletByID(customer.getPersonID());
 
         if (wallet == null) {
@@ -35,7 +47,7 @@ public class WalletManager {
 
 
     public boolean withdrawFunds(int amountCP, Customer customer) {
-        // WalletDAO walletDAO = new WalletDAO();
+        WalletDAO walletDAO = new WalletDAO();
         Wallet wallet = walletDAO.getWalletByID(customer.getPersonID());
 
         if (wallet == null) {
@@ -50,6 +62,42 @@ public class WalletManager {
 
         wallet.setCPbalance(wallet.getCPbalance() - amountCP);
         customer.setOwnWallet(wallet);
+        walletDAO.updateWallet(wallet);
+        return true;
+    }
+
+    //functions for testing
+    public void addFunds(int amountGP, int amountSP, int amountCP, int id) {
+        WalletDAO walletDAO = new WalletDAO();
+        Wallet wallet = walletDAO.getWalletByID(id);
+
+        if (wallet == null) {
+            System.out.println("Wallet not found for this customer.");
+            return;
+        }
+
+        int totalAmount = (amountGP*100) + (amountSP*10) + amountCP;
+
+        wallet.setCPbalance(wallet.getCPbalance() + totalAmount);
+        walletDAO.updateWallet(wallet);
+    }
+
+
+    public boolean withdrawFunds(int amountCP, int id) {
+        WalletDAO walletDAO = new WalletDAO();
+        Wallet wallet = walletDAO.getWalletByID(id);
+
+        if (wallet == null) {
+            System.out.println("Wallet not found for this customer.");
+            return false;
+        }
+
+        if (amountCP > wallet.getCPbalance()) {
+            System.out.println("You don't have enough funds to withdraw this amount.");
+            return false;
+        }
+
+        wallet.setCPbalance(wallet.getCPbalance() - amountCP);
         walletDAO.updateWallet(wallet);
         return true;
     }
