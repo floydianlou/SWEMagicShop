e214c44c924ee291c5aedabad2f71ba89c1b5cac
Novalynne <schirone.ma@gmail.com>
2025-03-15 19:49:18 +0100

    Created and Implemented Wallet DAO in ORM

---
+++ SWEMagicShopProject/src/ORM/WalletDAO.java
@@ -0,0 +1,59 @@
+package ORM;
+import DomainModel.*;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+
+public class WalletDAO {
+
+    private Connection connection;
+
+    public WalletDAO() {
+        try{
+            this.connection = ConnectionManager.getInstance().getConnection();;
+        }
+        catch(SQLException e){
+            System.out.println("Something happened while trying to connect to database..." + e.getMessage());
+        }
+    }
+
+    public Wallet getWalletByID(int ID) {
+        Wallet wallet = null;
+        String query = "SELECT * FROM \"Wallet\" WHERE customerID = ?";
+
+        try (PreparedStatement stmt = connection.prepareStatement(query)) {
+            stmt.setInt(1, ID);
+            try (ResultSet resultSet = stmt.executeQuery()) {
+                if (resultSet.next()) {
+                    int walletID = resultSet.getInt("customerID");
+                    int balanceCP = resultSet.getInt("CPbalance");
+                    wallet = new Wallet(walletID, balanceCP);
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println("Something happened while retrieving your wallet: " + e.getMessage());
+        }
+        return wallet;
+    }
+
+
+    public boolean updateWallet(Wallet wallet) {
+        String query = "UPDATE \"Wallet\" SET CPbalance = ? WHERE customerID = ?";
+
+        try (PreparedStatement stmt = connection.prepareStatement(query)) {
+            stmt.setInt(1, wallet.getCPbalance());
+            stmt.setInt(2, wallet.getWalletID());
+            int rowsAffected = stmt.executeUpdate();
+            if( rowsAffected > 0 ){
+                System.out.println("Wallet updated");
+                return true;
+            } else {
+                System.out.println("No rows were affected.");
+            }
+        } catch (SQLException e) {
+            System.err.println("Something happened while updating your wallet: " + e.getMessage());
+        }
+        return false;
+    }
+}
