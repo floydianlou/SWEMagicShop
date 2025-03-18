9e8c001bf62c9ca419a6aa9c06f3f36af6502547
floydianlou <yukiyesakura@gmail.com>
2025-03-18 20:52:42 +0100

    Added main and login views and controller for GUI
    All classes need to be completed, not yet fully functioning

--- SWEMagicShopProject/src/GUI/controller/MainViewController.java
+++ SWEMagicShopProject/src/GUI/controller/MainViewController.java
@@ -1,24 +1,44 @@
 package GUI.controller;
-
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+// TODO complete with register page actions
 
 public class MainViewController {
 
     @FXML
-    private Label label;
+    private ImageView iconImageView;
 
     @FXML
-    private Button button;
+    public void initialize() {
+        Image icon = new Image(getClass().getResource("/images/shop-icon.png").toExternalForm());
+        iconImageView.setImage(icon);
+    }
 
     @FXML
-    private void handleButtonClick() {
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Pagina di Login");
-        alert.setHeaderText(null);
-        alert.setContentText("Sezione Login");
-        alert.showAndWait();
+    private void handleLoginButton() {
+        loadScene("/GUI/view/login-view.fxml");
+    }
+
+    @FXML
+    private void handleRegisterButton() {
+        loadScene(""); //TODO
+    }
+
+    private void loadScene(String fxmlFile) {
+        try {
+            Stage stage = (Stage) iconImageView.getScene().getWindow();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFile));
+            Scene scene = new Scene(loader.load());
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- SWEMagicShopProject/src/main.java
+++ SWEMagicShopProject/src/main.java
@@ -1,24 +1,27 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class main extends Application {
-    @Override
-    public void start(Stage primaryStage) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/main-view.fxml"));
-        Parent root = loader.load();
-        Scene scene = new Scene(root);
 
-        primaryStage.setTitle("Otari Magic Shop");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    private static Stage primaryStage;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        primaryStage = stage;
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("GUI/view/main-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 400, 300);
+        stage.setTitle("Otari Magic Shop");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static Stage getPrimaryStage() {
+        return primaryStage;
     }
 
     public static void main(String[] args) {
-        launch(args);
+        launch();
     }
 }
